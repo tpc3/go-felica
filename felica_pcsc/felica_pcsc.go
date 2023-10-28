afdb887f286dc5122fc728c96770ef9496e99308
ozraru <ozraru@raru.work>
2023-10-28 18:53:19 +0900

    Change long type name

--- felica_pcsc/felica_pcsc.go
+++ felica_pcsc/felica_pcsc.go
@@ -16,7 +16,7 @@ var ErrUnknown = errors.New("unknown error")
 var ErrMasterKeyNil = errors.New("master key is nil")
 var ErrMacNotMatched = errors.New("mac_a didn't matched")
 
-type FeliCaLiteSwithPCSC struct {
+type FeliCaLiteS struct {
 	felica.FeliCaLiteS
 	Card *scard.Card
 }
@@ -51,8 +51,8 @@ func GetData(card *scard.Card, dataType DataType) ([]byte, error) {
 type MasterKeyProvider func([2]byte) *[24]byte
 
 // If masterKeyProvider is nil, MAC check skipped
-func NewCard(card *scard.Card, masterKeyProvider MasterKeyProvider) (*FeliCaLiteSwithPCSC, error) {
-	c := FeliCaLiteSwithPCSC{
+func NewCard(card *scard.Card, masterKeyProvider MasterKeyProvider) (*FeliCaLiteS, error) {
+	c := FeliCaLiteS{
 		Card: card,
 	}
 
@@ -102,7 +102,7 @@ func NewCard(card *scard.Card, masterKeyProvider MasterKeyProvider) (*FeliCaLite
 	return &c, nil
 }
 
-func (c *FeliCaLiteSwithPCSC) Read(address []byte) ([]felica.Block, error) {
+func (c *FeliCaLiteS) Read(address []byte) ([]felica.Block, error) {
 	blockList := make([]byte, 0, len(address)*2)
 	for _, v := range address {
 		blockList = append(blockList, 0x80)
@@ -132,7 +132,7 @@ func (c *FeliCaLiteSwithPCSC) Read(address []byte) ([]felica.Block, error) {
 }
 
 // address length: 1-3
-func (c *FeliCaLiteSwithPCSC) ReadWithMac(address []byte) ([]felica.Block, error) {
+func (c *FeliCaLiteS) ReadWithMac(address []byte) ([]felica.Block, error) {
 	resp, err := c.Read(append(address, felica.AddressMAC_A))
 	if err != nil {
 		return nil, err
@@ -145,7 +145,7 @@ func (c *FeliCaLiteSwithPCSC) ReadWithMac(address []byte) ([]felica.Block, error
 	}
 }
 
-func (c *FeliCaLiteSwithPCSC) Write(data []felica.Block) error {
+func (c *FeliCaLiteS) Write(data []felica.Block) error {
 	blockList := make([]byte, 0, len(data)*2)
 	dataArr := make([]byte, 0, len(data)*16)
 	for _, v := range data {
@@ -173,7 +173,7 @@ func (c *FeliCaLiteSwithPCSC) Write(data []felica.Block) error {
 	}
 }
 
-func (c *FeliCaLiteSwithPCSC) WriteWithMac(data felica.Block) error {
+func (c *FeliCaLiteS) WriteWithMac(data felica.Block) error {
 	resp, err := c.ReadWithMac([]byte{felica.AddressWCNT})
 	if err != nil {
 		return err
@@ -192,7 +192,7 @@ const (
 	ServiceRO = 0x000b
 )
 
-func (c *FeliCaLiteSwithPCSC) SetService(service uint16) error {
+func (c *FeliCaLiteS) SetService(service uint16) error {
 	command := []byte{0xFF, 0xA4, 0x00, 0x01, 0x02}
 	command = binary.LittleEndian.AppendUint16(command, service)
 	resp, err := c.Card.Transmit(command)
@@ -209,7 +209,7 @@ func (c *FeliCaLiteSwithPCSC) SetService(service uint16) error {
 	}
 }
 
-func (c *FeliCaLiteSwithPCSC) Command(command []byte) ([]byte, error) {
+func (c *FeliCaLiteS) Command(command []byte) ([]byte, error) {
 	base := []byte{0xFF, 0xFE, 0x00, 0x00, byte(len(command))}
 	return c.Card.Transmit(append(base, command...))
 }
